5690dad41fada4f390b4b2fa632a65cd55055eee
LindseyLamb <lgslamb@gmail.com>
2021-12-02 22:31:46 +0000

    moredevise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_02_220258) do
+ActiveRecord::Schema.define(version: 2021_12_02_222858) do
 
   create_table "projects", force: :cascade do |t|
     t.string "title"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 2021_12_02_220258) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.string "surname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
